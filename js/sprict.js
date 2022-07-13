70a547ffc1b9563117071d3d047baca5bfe3b132
minagi fujishima <22aw0134@jec.ac.jp>
2022-07-13 16:53:02 +0900

    1652

--- js/sprict.js
+++ js/sprict.js
@@ -5,3 +5,35 @@ ham.on('click', function () { //ハンバーガーメニューをクリックし
   nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
 
 });
+$(function () {
+  // スクロールしたら「トップに戻る」ボタンが表示される
+  const toTopButton = $(".jump");
+  const scrollHeight = 100;
+  toTopButton.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      toTopButton.fadeIn();
+    } else {
+      toTopButton.fadeOut();
+    }
+  });
+
+  // 「トップに戻る」ボタンをクリックしたらスクロールで戻る
+  toTopButton.click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 20);
+    return false;
+  });
+});
+$(function () {
+  // スクロールしたら「トップに戻る」ボタンが表示される
+  const coronaButton = $(".corona");
+  const scrollHeight = 100;
+  toTopButton.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      toTopButton.fadeIn();
+    } else {
+      toTopButton.fadeOut();
+    }
+  });
+});
